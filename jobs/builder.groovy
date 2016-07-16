b9926b0958f58f7a78b784a2faf827bbca83df45
Gergely Brautigam <skarlso777@gmail.com>
2016-07-16 17:19:42 +0200

    Added a job description.

---
+++ jobs/builder.groovy
@@ -0,0 +1,52 @@
+job('./TestJOb') {
+    description """Some <strong>strong</strong>,<br/>
+                 multiline text here.<br/>"""
+
+    label('master')
+    parameters {
+        stringParam('param1', 'default', 'First Parameter')
+        stringParam('param2', '', """Some<br/>
+                                  Multiline<br/>
+                                  <b>Text here as well.</b>""")
+        choiceParam('target_env', ['linux', 'osx'], 'Target environment')
+    }
+    wrappers {
+        preBuildCleanup {
+            deleteDirectories()
+        }
+        timestamps()
+    }
+    scm {
+        git {
+            remote {
+                name('main')
+                url('git@github.com:Skarlso/jenkinsbestpractices.git')
+            }
+        }
+    }
+
+    steps {
+        shell('go build $WORKSPACE/src/main.go')
+    }
+    publishers {
+        extendedEmail {
+            recipientList('me@halfempty.org')
+            defaultSubject('Oops')
+            defaultContent('Something broken')
+            contentType('text/html')
+            triggers {
+                beforeBuild()
+                stillUnstable {
+                    subject('Subject')
+                    content('Body')
+                    sendTo {
+                        developers()
+                        requester()
+                        culprits()
+                    }
+                }
+            }
+        }
+        buildDescription('', '${BRANCH}')
+    }
+}
